9dd9c4d2f8f62a85992495fcf7122332ead47d86
Eran <agmon.eran@gmail.com>
2024-08-07 12:48:52 -0400

    make skeleton MICOM process for vivarium

---
+++ process.py
@@ -0,0 +1,122 @@
+"""
+Vivarium process to run MICOM
+"""
+import numpy as np
+from vivarium.core.process import Process
+from vivarium.core.engine import Engine
+
+
+# from MICOM import ....  # TODO all required MICOM imports
+
+
+class MICOM(Process):
+
+    # defaults describes the parameters that the process expects
+    defaults = {
+        'species_files': None,
+    }
+
+    def __init__(self, parameters=None):
+        super().__init__(parameters)
+        # in the constructor we initialize all MICOM processes
+
+        # load the species files
+        self.parameters['species_files']
+        self.species_ids = []  # list of species ids
+
+        # get minimal media
+        self.media_molecules = []  # list of media molecules
+
+        # put asserts here to make sure all the relevant information is loaded
+        assert self.parameters['species_files'] is not None, 'species_files must be provided'
+
+    def ports_schema(self):
+        ports = {
+            'media': {  # media is the port name
+                mol_id: {
+                    '_default': np.array(),  # dic from nutrient id to concentrations
+                    '_updater': 'set',  # how the port is updated
+                    '_emit': True,  # whether the port is emitted
+                    # '_serializer': 'json',  # how the port is serialized
+                    # '_divider': 'split',
+                } for mol_id in self.media_molecules
+            },
+            'exchange_bounds': {},
+            'species_abundance': {
+                species_id: {
+                    '_default': 0.0,
+                    '_updater': 'set',
+                    '_emit': True,
+                } for species_id in self.species_ids
+            },
+            'community_growth_rate': {
+                '_default': 0.0,
+                '_updater': 'set',
+                '_emit': True,
+            },
+            'exchange fluxes': {},
+        }
+        return ports
+
+    def next_update(self, interval, states):
+        # get the media concentrations
+        media = states['media']
+
+        # get the species abundance
+        species_abundance = states['species_abundance']
+
+        # set the values in MICOM
+
+        # run MICOM
+        # TODO
+
+        # extract results
+        results = {}
+
+        return {
+            # 'media': results['media'],
+            'species_abundance': results['species_abundance'],
+            'community_growth_rate': results['community_growth_rate'],
+            'exchange fluxes': results['exchange fluxes'],
+            # 'results': results,
+        }
+
+
+
+
+def run_process(total_time=10):
+
+    # create a config
+    config = {
+        'species_files': ['models/ecoli.json',],
+    }
+    # create a MICOM process
+    micom_process = MICOM(config)
+
+    # define the composite
+    processes = {
+        'micom': micom_process,
+    }
+    topology = {
+        'micom': {
+            'media': ('media', 'micom'),
+            'species_abundance': ('species_abundance', 'micom'),
+            'community_growth_rate': ('community_growth_rate', 'micom'),
+        }
+    }
+
+    # make a vivarium simulation
+    sim = Engine(processes=processes, topology=topology)
+
+    # run the simulation
+    sim.update(total_time)
+
+    # get the data
+    data = sim.emitter.get_data()
+
+    print(data)
+
+
+
+if __name__ == '__main__':
+    run_process()
