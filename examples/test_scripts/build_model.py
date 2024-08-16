4bcb400986ff81f0ee7d8b36f90fa422aed2a044
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2024-08-16 18:46:40 -0400

    micom model build code

--- examples/test_scripts/build_model.py
+++ examples/test_scripts/build_model.py
@@ -10,41 +10,43 @@ from micom.workflows import workflow
 import os
 
 
+#%%
+# wd_default = os.path.dirname(os.getcwd())
+#
+# os.chdir(wd_default)
+# print(wd_default)
+# print(os.getcwd())
 
-wd_default = os.path.dirname(os.getcwd())
+path_data = os.path.join(os.getenv("HOME"),'projects','micom_validation','paper','data')
 
-os.chdir(wd_default)
-print(wd_default)
-print(os.getcwd())
+path_model = os.path.join(os.getcwd(),'models')
+
 #%%
 
 
 logger = micom.logger.logger
-try:
-    max_procs = snakemake.threads
-except NameError:
-    max_procs = 20
 
-makedirs("data/models", exist_ok=True)
+
+# makedirs("data/models", exist_ok=True)
 
 # print(os.getcwd())
 
-taxonomy = pd.read_csv("data/genera.csv").query("relative > 1e-3")
+taxonomy = pd.read_csv(os.path.join(path_data,"genera.csv")).query("relative > 1e-3")
 taxonomy["file"] = taxonomy.file.apply(
-    lambda ids: ["data/agora/" + i for i in ids.split("|")]
+    lambda ids: [os.path.join(path_data,"agora")+"/" + i for i in ids.split("|")]
 )
 taxonomy["name"] = taxonomy.genus
 assert not taxonomy.name.str.contains(" ").any()
 taxonomy = taxonomy.rename(columns={"name": "id", "reads": "abundance"})
 
-diet = pd.read_csv("data/western_diet.csv")
+diet = pd.read_csv(os.path.join(path_data,"western_diet.csv"))
 diet.index = diet.reaction = diet.reaction.str.replace("_e", "_m")
 diet = diet.flux * diet.dilution
 
 
 def build_and_save(args):
     s, tax = args
-    filename = "data/models/" + s + ".pickle"
+    filename = "models/" + s + ".pickle"
     if isfile(filename):
         return
     com = Community(tax, id=s, progress=False)
@@ -62,14 +64,16 @@ def build_and_save(args):
 samples = taxonomy.samples.unique()
 args = [(s, taxonomy[taxonomy.samples == s]) for s in samples]
 
-#%% build model test
-
-build_and_save(args[49])
 
 
-#%%
-#
-# workflow(build_and_save, args[1:16], max_procs)
+#%% load the first patient sample
 
+sample_0 = samples[0]
 
+# define input to build function with fist 5 genera of first patient sample
 
+args_0 = (str(sample_0)+'_0', taxonomy[taxonomy.samples == samples[0]].iloc[:5,:])
+
+#%% build micom toy model
+
+build_and_save(args_0)
