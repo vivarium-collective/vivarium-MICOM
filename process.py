a2f371c1cfa770bda31ecd049d74310d9101b903
arnabmutsuddy <arnab.mutsuddy@gmail.com>
2024-08-26 09:39:11 -0400

    fluxes columns

--- process.py
+++ process.py
@@ -16,9 +16,9 @@ from micom.logger import logger
 
 #%% temp - micom model test bench
 
-sample_id = 'ERR260132'
-
-com = load_pickle("models/" + sample_id + ".pickle")
+# sample_id = 'ERR260132'
+#
+# com = load_pickle('models/' + sample_id + '.pickle')
 
 #%%
 
@@ -32,7 +32,7 @@ class MICOM(Process):
 
     # defaults describes the parameters that the process expects
     defaults = {
-        'patient_model': 'ERR260132'
+        'patient_model': 'ERR260132',
         'species_files': None,
     }
 
@@ -41,8 +41,8 @@ class MICOM(Process):
         # in the constructor we initialize all MICOM processes
 
         # load the species files
-        self.com = load_pickle('models/'+str(self.parameters['patient_model'])+'.pkl')
-        self.parameters['species_files']
+        self.com = load_pickle('models/'+str(self.parameters['patient_model'])+'.pickle')
+        #self.parameters['species_files']
         self.species_ids = list(self.com.taxonomy['id'])  # list of species ids
         self.exchanges = [exc.id for exc in self.com.exchanges]
         self.internal_exchanges = [iexc.id for iexc in self.com.internal_exchanges]
@@ -51,38 +51,39 @@ class MICOM(Process):
         self.media_molecules = list(self.com.medium.keys())  # list of media molecules
 
         # put asserts here to make sure all the relevant information is loaded
-        assert self.parameters['species_files'] is not None, 'species_files must be provided'
+
 
     def ports_schema(self):
         ports = {
             'media': {  # media is the port name
                 mol_id: {
-                    '_default': 0.0,  # dic from nutrient id to concentrations
+                    '_default': 1000.0,  # dic from nutrient id to concentrations
                     '_updater': 'set',  # how the port is updated
                     '_emit': True,  # whether the port is emitted
                     # '_serializer': 'json',  # how the port is serialized
                     # '_divider': 'split',
                 } for mol_id in self.media_molecules
             },
-            'exchange_bounds': {},
-            'species_abundance': {
-                str(species_id): {
-                    '_default': 0.0,
-                    '_updater': 'set',
-                    '_emit': True,
-                } for species_id in self.species_ids
-            },'growth_rates': {
-                str(species_id):{
-                '_default': 0.0,
-                '_updater': 'set',
-                '_emit': True,
-            } for species_id in self.com.abundances.index
-        },
-            'community_growth_rate': {
-                '_default': 0.0,
-                '_updater': 'set',
-                '_emit': True,
-            },
+            # 'exchange_bounds': {},
+            # 'species_abundance': {
+            #     str(species_id): {
+            #         '_default': 0.0,
+            #         '_updater': 'set',
+            #         '_emit': True,
+            #     } for species_id in self.species_ids
+            # },
+            # 'growth_rates': {
+            #     str(species_id):{
+            #     '_default': 0.0,
+            #     '_updater': 'set',
+            #     '_emit': True,
+            #     } for species_id in self.com.abundances.index
+            # },
+            # 'community_growth_rate': {
+            #     '_default': 0.0,
+            #     '_updater': 'set',
+            #     '_emit': True,
+            # },
             'fluxes': {
                 str(rxn): {
                     '_default': 0.0,  # dic from nutrient id to concentrations
@@ -98,47 +99,94 @@ class MICOM(Process):
         media_input = states['media']
 
         # get the species abundance
-        species_abundance = states['species_abundance']
+        # species_abundance = states['species_abundance']
 
         # set the values in MICOM
 
         # run MICOM
         # TODO
 
-        com.medium = media_input[media_input > 0]
-        sol = com.cooperative_tradeoff(fraction=0.5, fluxes=True, pfba=False)
+        self.com.medium = media_input
+        sol = self.com.cooperative_tradeoff(fraction=0.5, fluxes=True, pfba=False)
         fluxes_sol = sol.fluxes
 
         fluxes = {}
 
         for rxn in self.reactions:
-            flux_search = rxn.split('__')
-            if len(flux_search) == 2:
-                fluxes[rxn] = fluxes_sol.loc[flux_search[1],flux_search[0]]
-            elif len(flux_search) == 1:
+
+            rxn_spc = list(filter(lambda x: x in rxn,self.species_ids))
+
+            if len(rxn_spc) !=0:
+                spc = rxn_spc[0]
+                rxn_id = rxn.replace('__'+spc,'')
+
+                fluxes[rxn] = fluxes_sol.loc[spc,rxn_id]
+            else:
                 fluxes[rxn] = fluxes_sol.loc['medium',rxn]
 
-        growth_rates = {
-            str(species_id):
-                sol.members["growth_rate"][str(species_id)]
-            for species_id in self.species_ids
-        }
+            # flux_search = rxn.split('__')
+            # if len(flux_search) == 2:
+            #     fluxes[rxn] = fluxes_sol.loc[flux_search[1],flux_search[0]]
+            # elif len(flux_search) == 1:
+            #     fluxes[rxn] = fluxes_sol.loc['medium',rxn]
+
+        # growth_rates = {
+        #     str(species_id):
+        #         sol.members["growth_rate"][str(species_id)]
+        #     for species_id in self.species_ids
+        # }
 
 
         return {
-            'growth_rates': growth_rates,
-            'community_growth_rate': sol.growth_rate,
+            # 'growth_rates': growth_rates,
+            # 'community_growth_rate': sol.growth_rate,
             'fluxes': fluxes
         }
 
 
+class media_update(Process):
+    defaults = {}
 
+    def __init__(self,parameters=None):
 
-def run_process(total_time=10):
+        super().__init__(parameters)
+
+        self.com = load_pickle('models/'+str(self.parameters['patient_model'])+'.pkl')
+        self.media_molecules = list(self.com.medium.keys())
+
+    def ports_schema(self):
+        ports = {
+            'media': {
+                mol_id: {
+                    '_default': 0.0,
+                    '_updater': 'set',
+                    '_emit': True,
+                } for mol_id in self.media_molecules
+            }
+        }
+
+        return ports
+
+    def next_update(self, interval, states):
+
+        media_input = states['media']
+
+        v_max = 1.00
+        k_m = 1.00
+
+        media_output = media_input.values*v_max/(k_m+media_input.values)
+
+        return {
+            'media': {
+                media_output
+            }
+        }
+
+def run_process(total_time=5):
 
     # create a config
     config = {
-        'species_files': ['models/ecoli.json',],
+        # 'species_files': ['models/ecoli.json',],
     }
     # create a MICOM process
     micom_process = MICOM(config)
@@ -149,9 +197,12 @@ def run_process(total_time=10):
     }
     topology = {
         'micom': {
-            'media': ('media', 'micom'),
-            'species_abundance': ('species_abundance', 'micom'),
-            'community_growth_rate': ('community_growth_rate', 'micom'),
+            'fluxes': ('fluxes_store',),
+            'media': ('media_store',),
+
+            #     'media': ('media', 'micom'),
+        #     'species_abundance': ('species_abundance', 'micom'),
+        #     'community_growth_rate': ('community_growth_rate', 'micom'),
         }
     }
 
@@ -170,3 +221,27 @@ def run_process(total_time=10):
 
 if __name__ == '__main__':
     run_process()
+
+#%%
+# config = {
+# }
+# # create a MICOM process
+# micom_process = MICOM(config)
+#
+# # define the composite
+# processes = {
+#     'micom': micom_process,
+# }
+# topology = {
+#     'micom': {
+#         'fluxes': ('fluxes_store',),
+#         'media': ('media_store',),
+#     }
+# }
+#
+# # make a vivarium simulation
+# sim = Engine(processes=processes, topology=topology)
+#
+# # run the simulation
+# sim.update(5)
+#%%
